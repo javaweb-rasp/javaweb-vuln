37ab4b91e250a925a3470ad3530ebd21c8e7568b
yz <admin@javaweb.org>
2022-03-30 22:28:33 +0800

    添加XSS用例

--- vuln-core/src/main/java/org/javaweb/vuln/controller/SQLInjectionController.java
+++ vuln-core/src/main/java/org/javaweb/vuln/controller/SQLInjectionController.java
@@ -2,7 +2,10 @@ package org.javaweb.vuln.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
--- vuln-core/src/main/java/org/javaweb/vuln/controller/XSSController.java
+++ vuln-core/src/main/java/org/javaweb/vuln/controller/XSSController.java
@@ -1,16 +1,30 @@
 package org.javaweb.vuln.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
+
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
 @Controller
 @RequestMapping("/XSS/")
 public class XSSController {
 
+	@ResponseBody
 	@RequestMapping("/callback.do")
-	public @ResponseBody String callbackXSS(String str) {
+	public String callbackXSS(String str) {
 		return str;
+	}
+
+	@ResponseBody
+	@PostMapping(value = "/xss.do", consumes = APPLICATION_JSON_VALUE)
+	public Map<String, Object> xss(@RequestBody Map<String, Object> map) {
+		map.put("msg", "请求成功！");
+		return map;
 	}
 
 }
